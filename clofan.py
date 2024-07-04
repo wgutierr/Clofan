39a0512e73646e6fc99aa44a009e7ad7bca9de16
Wilfer Gutierrez <wgutierr@eafit.edu.co>
2024-07-03 19:37:30 -0500

    Matplotlib requierement
    Se elimina Matplotlib de los requerimientos

--- clofan.py
+++ clofan.py
@@ -7,7 +7,6 @@
 import plotly.express as px
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import plotly.io as pio
 import plotly.graph_objects as go
 import streamlit as st
